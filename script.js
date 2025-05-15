c19985cc14d7ea43288f962bca999a12f995e2bd
axellpkmn <axelivanlascano@gmail.com>
2025-05-15 13:40:01 +0200

    prueba funcionando perfectamente

--- script.js
+++ script.js
@@ -1,30 +1,80 @@
 document.addEventListener('DOMContentLoaded', function () {
-  const video = document.getElementById('background-video');
-  video.pause();
-  video.currentTime = 0;
+  const video1 = document.getElementById('background-video')
+  video1.pause();
+  video1.currentTime = 0;
+  const videoReversa = document.getElementById('background-video-reversa')
+  videoReversa.pause();
+  videoReversa.currentTime = 0;
 
-  function reproducirVideo() {
+  function reproducirVideo(video) {
     video.playbackRate = 2.0;
     video.play();
   }
 
-  document.getElementById('item1').addEventListener('click', function() {
+
+  const goAbout = () => {
+    setTimeout(function () {
       let home = document.querySelector('.inventario-home');
       let about = document.querySelector('.inventario-about');
       let volver = document.querySelector('#volver-home');
-    home.classList.add('hidden');
-    about.classList.remove('hidden');
-    volver.classList.remove('hidden');
-});
+      home.classList.add('hidden');
+      about.classList.remove('hidden');
+      volver.classList.remove('hidden');
+      video1.classList.add('hidden');
+      videoReversa.classList.remove('hidden');
+      video1.currentTime = 0;
+    }, 2300);
+  }
+  
+  const goBackMenu = () => {
+    setTimeout(function () {
+      let home = document.querySelector('.inventario-home');
+      let about = document.querySelector('.inventario-about');
+      let volver = document.querySelector('#volver-home');
+      const video1 = document.getElementById('background-video')
+      home.classList.remove('hidden');
+      about.classList.add('hidden');
+      volver.classList.add('hidden');
+      video1.classList.remove('hidden');
+      videoReversa.classList.add('hidden');
+      videoReversa.currentTime = 0;
+    }, 2300);
+  }
+
+
+
+  document.getElementById('volver-home').addEventListener('click', function () {
+    reproducirVideo(videoReversa);
+    goBackMenu();
+    
+  });
 
   // Eventos para el video
-  document.getElementById('item1').addEventListener('click', reproducirVideo);
-  document.getElementById('item2').addEventListener('click', reproducirVideo);
-  document.getElementById('item3').addEventListener('click', reproducirVideo);
-  document.getElementById('item4').addEventListener('click', reproducirVideo);
+  document.getElementById('item1').addEventListener('click', () => {
+    reproducirVideo(video1);
+    goAbout();
+  });
+  // document.getElementById('item2').addEventListener('click', () => {
+  //   reproducirVideo();
+  //   goMenu();
+  // });
+  // document.getElementById('item3').addEventListener('click', () => {
+  //   reproducirVideo();
+  //   goMenu();
+  // });
+  // document.getElementById('item4').addEventListener('click', () => {
+  //   reproducirVideo();
+  //   goMenu();
+  // });
   // Volver al home
-  document.getElementById('volver-home').addEventListener('click', reproducirVideo);
+  document.getElementById('volver-home').addEventListener('click', () => {
+    reproducirVideoReversa();
+  });
 });
+
+
+
+
 
 
 
