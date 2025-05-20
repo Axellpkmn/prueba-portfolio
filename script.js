1d81266bb374cf23af0da48225bb305649b3879a
axellpkmn <axelivanlascano@gmail.com>
2025-05-20 13:29:57 +0200

    servicios terminado

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
 document.addEventListener('DOMContentLoaded', function () {
   const video1 = document.getElementById('background-video')
   video1.pause();
-  video1.currentTime = 0;
   const videoReversa = document.getElementById('background-video-reversa')
   videoReversa.pause();
-  videoReversa.currentTime = 0;
 
   function reproducirVideo(video) {
     video.playbackRate = 3.0;
@@ -23,6 +21,20 @@ document.addEventListener('DOMContentLoaded', function () {
       video1.classList.add('hidden');
       videoReversa.classList.remove('hidden');
       video1.currentTime = 0;
+    }, 1700);
+  }
+
+  const goServices = () => {
+    setTimeout(function () {
+      let home = document.querySelector('.inventario-home');
+      let services = document.querySelector('.inventario-servicios');
+      let volver = document.querySelector('#volver-home');
+      home.classList.add('hidden');
+      services.classList.remove('hidden');
+      volver.classList.remove('hidden');
+      video1.classList.add('hidden');
+      videoReversa.classList.remove('hidden');
+      video1.currentTime = 0;
     }, 1600);
   }
   
@@ -30,10 +42,12 @@ document.addEventListener('DOMContentLoaded', function () {
     setTimeout(function () {
       let home = document.querySelector('.inventario-home');
       let about = document.querySelector('.inventario-about');
+      let services = document.querySelector('.inventario-servicios');
       let volver = document.querySelector('#volver-home');
       const video1 = document.getElementById('background-video')
       home.classList.remove('hidden');
       about.classList.add('hidden');
+      services.classList.add('hidden');
       volver.classList.add('hidden');
       video1.classList.remove('hidden');
       videoReversa.classList.add('hidden');
@@ -54,21 +68,10 @@ document.addEventListener('DOMContentLoaded', function () {
     reproducirVideo(video1);
     goAbout();
   });
-  // document.getElementById('item2').addEventListener('click', () => {
-  //   reproducirVideo();
-  //   goMenu();
-  // });
-  // document.getElementById('item3').addEventListener('click', () => {
-  //   reproducirVideo();
-  //   goMenu();
-  // });
-  // document.getElementById('item4').addEventListener('click', () => {
-  //   reproducirVideo();
-  //   goMenu();
-  // });
-  // Volver al home
-  document.getElementById('volver-home').addEventListener('click', () => {
-    reproducirVideoReversa();
+
+  document.getElementById('item2').addEventListener('click', () => {
+    reproducirVideo(video1);
+    goServices();
   });
 });
 
